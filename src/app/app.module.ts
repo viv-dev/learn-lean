1fd2bd213a6bf4443f22d917883d0b40f333d679
Vivian Capote <viv.dev@protonmail.com>
2021-10-16 11:26:37 +1100

    Added firebase and fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'learn-lean',
+    appId: '1:423679698599:web:103672e35bea39c15f5d82',
+    storageBucket: 'learn-lean.appspot.com',
+    apiKey: 'AIzaSyAF6Idt9EQs34TXKGLjARWifjWj_Inknc0',
+    authDomain: 'learn-lean.firebaseapp.com',
+    messagingSenderId: '423679698599',
+  },
   production: true
 };
